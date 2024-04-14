38de17d7512dc5bd00b86283628253d99d5b098d
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-04-14 14:12:54 +0800

    Add: Sorting visualizer component and stylesheet

---
+++ src/components/SortingVisualizer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./SortingVisualizer.css";
+
+export default class SortingVisualizer extends React.Component {
+    render() {
+        return (
+            <>
+            
+            </>
+        )
+    }
+}
