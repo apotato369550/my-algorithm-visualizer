d346dc2049f860d394b6620d677b29072a4bece1
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-04-15 12:24:52 +0800

    Add: Sorting Visualizer Component

--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -2,11 +2,41 @@ import React from 'react';
 import "./SortingVisualizer.css";
 
 export default class SortingVisualizer extends React.Component {
-    render() {
-        return (
-            <>
-            
-            </>
-        )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      array: [],
+    };
+  }
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  randomIntFromInterval(min, max) {
+    Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
+  resetArray() {
+    const array = [];
+    for (let i = 0; i < 100; i++) {
+      array.push(this.randomIntFromInterval(5, 1000));
     }
+    this.useState({ array });
+  }
+
+  render() {
+    const { array } = this.state;
+
+    return (
+      <>
+        {array.map((value, idx) => {
+          <div className="array-bar" key={idx}>
+            {value}
+          </div>;
+        })}
+      </>
+    );
+  }
 }
