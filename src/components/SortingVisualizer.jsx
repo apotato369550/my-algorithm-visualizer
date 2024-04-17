76bcacdfce9b2ff00f0e75cd8cd0efa19c2bc1b1
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-04-17 14:16:57 +0800

    Edit: Made it display the value of each bar

--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -15,27 +15,28 @@ export default class SortingVisualizer extends React.Component {
   }
 
   randomIntFromInterval(min, max) {
-    Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
   
   resetArray() {
     const array = [];
     for (let i = 0; i < 100; i++) {
-      array.push(this.randomIntFromInterval(5, 1000));
+      let randomInteger = this.randomIntFromInterval(5, 1000);
+      array.push(randomInteger);
     }
-    this.useState({ array });
+    this.setState({ array });
   }
 
   render() {
     const { array } = this.state;
-
+  
     return (
       <>
-        {array.map((value, idx) => {
+        {array.map((value, idx) => (
           <div className="array-bar" key={idx}>
             {value}
-          </div>;
-        })}
+          </div>
+        ))}
       </>
     );
   }
