49a1ce784d69eeb26befa1b13de5a359fe17456b
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-04-22 09:34:54 +0800

    Worked on Mergesort Algorithm

--- src/sortingAlgorithms/sortingAlgorithms.js
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -1,3 +1,8 @@
 export const mergeSort = array => {
-    // do mergesort here
+    if(array.length === 1) return array;
+    const middleIndex = Math.floor(array.length / 2);
+    const firstHalf = mergeSort(array.slice(0, middleIndex));
+    const secondHalf = mergeSort(array.slice(middleIndex));
+
+    // do algorithm here T_T
 }
