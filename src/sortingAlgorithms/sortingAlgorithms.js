103182e9c2ab3caca44ca97e4ded091915093dc0
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-05-01 13:07:55 +0800

    Worked on doMerge function

--- src/sortingAlgorithms/sortingAlgorithms.js
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -1,5 +1,5 @@
-export const getMergeSortAnimations(array) {
-    animations = [];
+export function getMergeSortAnimations(array) {
+    const animations = [];
     if (array.length <= 1) return array;
     const auxiliaryArray = array.slice();
     mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations)
@@ -11,7 +11,7 @@ function mergeSortHelper(mainArray, startIndex, endIndex, auxiliaryArray, animat
     const middleIndex = Math.floor((startIndex + endIndex) / 2);
     mergeSortHelper(auxiliaryArray, startIndex, middleIndex, mainArray, animations);
     mergeSortHelper(auxiliaryArray, middleIndex + 1, endIndex, mainArray);
-    doMerge(mainArray, startIndex, middleIndex, endIndex, auxiliaryArray, animiations)
+    doMerge(mainArray, startIndex, middleIndex, endIndex, auxiliaryArray, animations)
 }
 
 function doMerge(mainArray, startIndex, middleIndex, endIndex, auxiliaryArray, animations) {
@@ -27,7 +27,18 @@ function doMerge(mainArray, startIndex, middleIndex, endIndex, auxiliaryArray, a
             mainArray[k++] = auxiliaryArray[j++]
         }
     }
-
+    while (i <= middleIndex) {
+        animations.push([i, i]);
+        animations.push([i, i]);
+        animations.push([k, auxiliaryArray[i]]);
+        mainArray[k++] = auxiliaryArray[i++];
+    }
+    while (j <= endIndex) {
+        animations.push([j, j]);
+        animations.push([j, j]);
+        animations.push([k, auxiliaryArray[j]]);
+        mainArray[k++] = auxiliaryArray[j++];
+    }
     // continue here
 }
 
