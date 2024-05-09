73d55f1663dcab2214f7c60449a4d869847f443e
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-05-09 11:12:57 +0800

    Started on Bubblesort animations

--- src/sortingAlgorithms/sortingAlgorithms.js
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -1,3 +1,11 @@
+export function bubbleSort(array) {
+    const animations = [];
+    if (array.length <= 1) return array;
+    // do some stuff here
+
+    return animations
+}
+
 export function mergeSort(array) {
     const animations = [];
     if (array.length <= 1) return array;
@@ -43,27 +51,3 @@ function doMerge(mainArray, startIndex, middleIndex, endIndex, auxiliaryArray, a
         mainArray[k++] = auxiliaryArray[j++];
     }
 }
-
-/*
-
-export const mergeSort = (array, startIndex, endIndex, animations = []) => {
-    if(array.length === 1) return array;
-    const middleIndex = Math.floor(array.length / 2);
-    const firstHalf = mergeSort(array, startIndex, middleIndex, animations);
-    const secondHalf = mergeSort(array, middleIndex + 1, endIndex, animations);
-    const sortedArray = [];
-
-    let i = 0, j = 0;
-    while (i < firstHalf.length && j < secondHalf.length) {
-        if(firstHalf[i] < secondHalf[j]) {
-            sortedArray.push(firstHalf[i++]);
-        } else {
-            sortedArray.push(secondHalf[j++]);
-        }
-    }
-    while (i < firstHalf.length) sortedArray.push(firstHalf[i++])
-    while (j < secondHalf.length) sortedArray.push(secondHalf[j++]);
-    return sortedArray;
-}
-
-*/
