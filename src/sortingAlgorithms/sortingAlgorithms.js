985de2ffb1096eb6c5e43395f7fa8dc8a2c9d548
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-05-12 09:28:01 +0800

    Worked on bubblesort helper

--- src/sortingAlgorithms/sortingAlgorithms.js
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -2,8 +2,21 @@ export function bubbleSort(array) {
     const animations = [];
     if (array.length <= 1) return array;
     // do some stuff here
-
+    bubbleSortHelper(array, animations)
     return animations
+}
+
+function bubbleSortHelper(array, animations) {
+    for(let i = 0; i < array.length - 1; i++) { 
+        for (let j = 0; j < array.length - i - 1; j++) {
+            const animation = {}
+            animation.comparison = [i, j]
+            if (array[j] > array[j + 1]) {
+                // do stuff here
+            }
+            animations.push(animation)
+        }
+    }
 }
 
 export function mergeSort(array) {
